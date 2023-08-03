4d5d55d9c456128d5e19e9b28d2bd423e96ea1d5
Samaira Singh <singh.samaira101@gmail.com>
2023-08-04 00:11:08 +0530

    week-4 completed

---
+++ Week-4/Day-19/isMirror.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node==NULL) return ;
+        // code here
+        Node *temp=node->left;
+        node->left=node->right;
+        node->right=temp;
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
+
